71180ca04f787ca2703219a4d884db0479c07bb0
김멍몽 <wjdahrrla@gmail.com>
2023-11-12 03:51:16 +0900

    최종본

--- src/main/frontend/FrontEnd_Bunli-main/src/pages/giftDetail/index.js
+++ src/main/frontend/FrontEnd_Bunli-main/src/pages/giftDetail/index.js
@@ -40,7 +40,7 @@ const GiftDetail = () => {
   return (
       <div>
         <MenuBar />
-        <dliv className="giftshopTop">
+        <dlㅜㅔㅡiv className="giftshopTop">
           <Link
               to="/giftshop"
               style={{
@@ -52,7 +52,7 @@ const GiftDetail = () => {
             <i class="fa-solid fa-gift"></i>
             기프티콘 샵
           </Link>
-        </dliv>
+        </dlㅜㅔㅡiv>
 
         <div className="giftDetailTopClass">
           <img src={giftDetail.productImg} alt="image" />
